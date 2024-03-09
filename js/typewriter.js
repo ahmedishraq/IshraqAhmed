a486365de55e7a73611e78add6c1b670c9116c50
ahmedishraq <ishraqahmedbd@gmail.com>
2024-03-09 21:47:10 +0600

    About me update

--- js/typewriter.js
+++ js/typewriter.js
@@ -1,4 +1,4 @@
-var content = 'Interested to work in the computer software industry alongside interested to work in Artificial Intelligence (AI) sector as well . Skilled in, OOP (Object Oriented Programming), JAVA, Python, Data Analysis, Data Visualization, Microsoft Office, Event Management, IT Strategy, and Problem Solving . I have completed my Bachelor of Science (B.Sc) degree with a major in Computer Science and Engineering (CSE) from BRAC University.';
+var content = 'Experienced Software Engineer of Quality Assurance with a demonstrated history of working in the computer software industry. Skilled in Java, Python, SQL, Selenium, Automation Testing, Manual Testing, and Web Development. Enthusiast in the field of Machine Learning, Emotion AI, and Mobile Application Development. Strong years of Engineering professional Teaching experience with a Bachelor of Science (B.Sc.) focused on Computer Science and Engineering from BRAC University as a Teaching Assistant/ Student Tutor.';
 
 var ele = '<span>' + content.split('').join('</span><span>') + '</span>';
 
